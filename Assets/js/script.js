719488250fbb77fb5438abbfc2c3bba5ff0f73b8
Deanna <xander.deanna@gmail.com>
2021-01-23 18:54:51 -0600

    updating JS, added new folders for sounds, and updates Readme. Highscores is still broken

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,48 @@
-//quiz funtion //
+// global variables
+var score = 0;
+var questionIndex = 0;
 
-// quiz questions //
+// html DOM elements
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
+//variables for the timer
+var timer = document.querySelector("#start");
+var currentTime = document.querySelector("#currentTime");
+var secondsLeft = 61;
+var holdInterval = 0;
+var penalty = 5;
+var ulCreate = document.createElement("ul");
+
+// sounds linked through the html document
+var wrongSound = document.getElementById("wrongSound");
+var rightSound = document.getElementById("rightSound");
+var endSound = document.getElementById("endSound");
+
+// displays timer
+start.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "00:" + secondsLeft;
+            if (secondsLeft < 10) {
+                currentTime.textContent = "00:0" + secondsLeft;
+            }
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                gameOver();
+                endSound.play();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
+// questions, choices, and answers in an array
 var questions = [
     {
-        title: "_____ is a very useful tool for used for debugging and printing content to the debugger:",
+        title: "_____ is a tool for used for debugging and printing content to the debugger:",
         choices: ["javaScript", "terminal / bash", "for loops", "console log"],
         answer: "console log"
     },
@@ -34,38 +73,148 @@ var questions = [
     },
 ];
 
-
-// ties timer to start click //
-document.getElementById("start").onclick = function () {
-    var sixtySeconds = 30,
-        display = document.querySelector('.timer');
-    startTimer(sixtySeconds, display);
+// renders questions and choices to page
+function render(questionIndex) {
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    
+    // to loop through entire questions array
+    for (var i = 0; i < questions.length; i++) {
+        // appends question title only
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+    // appends questions and choices, and adds hr elements for aesthetics
+    var rule = document.createElement("hr");
+    questionsDiv.appendChild(rule);
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        var listBtn = document.createElement("btn")
+        questionsDiv.appendChild(ulCreate);
+        listBtn.textContent = newItem;
+        ulCreate.appendChild(listItem);
+        listItem.appendChild(listBtn);
+        listBtn.setAttribute("class", "btn");
+        listBtn.addEventListener("click", (compare));
+    });
+    var rule = document.createElement("hr");
+    questionsDiv.appendChild(rule);
 };
 
-// timer funtion //
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+// compares the questions to choices (and evaluates if correct). 
+// also plays sounds based on correctness
+function compare(event) {
+    var element = event.target;
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+    if (element.matches("btn")) {
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        // correct condition 
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+            rightSound.play();
+        // incorrect condition 
+        } else {
+            // Will deduct -5 seconds off secondsLeft for wrong answers
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+            wrongSound.play();
+        };
+    };
 
-        display.textContent = minutes + ":" + seconds;
+    // Question Index determines number question user is on
+    questionIndex++;
+    if (questionIndex >= questions.length) {
+        // All done will append last page with user stats
+        gameOver();
+        endSound.play();
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+    } else {
+        render(questionIndex);
+    };
+    questionsDiv.appendChild(createDiv);
+};
 
-        if (--timer < 0) {
-            display.textContent = "Times up!";
+// appends last page
+function gameOver() {
+    questionsDiv.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    // Heading:
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "Game Over!"
+
+    questionsDiv.appendChild(createH1);
+
+    // Paragraph
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionsDiv.appendChild(createP);
+
+    // Calculates time remaining and replaces it with score
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(holdInterval);
+        createP.textContent = "Your final score is: " + timeRemaining;
+
+        questionsDiv.appendChild(createP2);
+    }
+
+    // Label
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+    questionsDiv.appendChild(createLabel);
+
+    // input
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questionsDiv.appendChild(createInput);
+
+    // submit
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.setAttribute("class", "btn");
+    createSubmit.textContent = "Submit";
+
+    questionsDiv.appendChild(createSubmit);
+
+    // Event listener to capture initials and local storage for initials and score
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("No value entered!");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+            // Travels to final page
+            window.location.replace("./HighScores.html");
         }
-    }, 1000);
+    });
+
 }
-
-
-// Not working
-//  var quizContent = document.getElementsByTagName('quiz-box')[0],
-//     questionContent = document.createElement('h3');
-//     newQuestion.id = 'question';
-//     body.appendChild(newQuestion);
-//     choicesContent = document.createElement('p');
-//     newQuestion.id = 'choices';
-//     body.appendChild(newQuestion);
--- README.md
+++ README.md
@@ -2,15 +2,19 @@ code-quiz - Homework assignment for 01/20/2021
 
 ---------------------------------------------------------
 
-This week's assignment was to take a timed quiz on JavaScript fundamentals that stores high scores.
+This week's assignment was to make a timed quiz on JavaScript fundamentals that stores high scores.
 
-I started by building the bare bones of my site using HTML and javaScript. I also through in some HTML elements to help me visualize formatting, although many would be deleted later and created dynamically through javaScript. 
+I started by building the bare bones of my site using HTML and javaScript. I also threw in some HTML elements to help me visualize formatting, although many would be deleted later and created dynamically through javaScript. 
 
-After the HTML and CSS was complete, I started working on the javaScript. I started first with my timer. I tied the timer to a navbar element and made sure that it was displaying correctly. I also had the timer display the text "Times up!" once the time had reached 0.
+After the HTML and CSS was complete, I started working on the javaScript. I started first with my timer. I tied the timer to a navbar element and made sure that it was displaying correctly. I also had the timer display the text "Times up!" once the time had reached 0. I had some trouble with this timer since I decided I wanted to format to display as 00:XX. I set an if statement to display a string of "00:0" + time, once time remaining was below 10 (single digit). Otherwise a string of "00:" + time would display. 
 
-After I was happy with my timer I went ahead and started thinking of quiz questions. I decided the easiest way to go about this in my opinion, would be to create an array of objects.
+After I was happy with my timer I went ahead and started thinking of quiz questions. I decided the easiest way to go about this in my opinion, would be to create an array of objects. This for me personally was the easiest part of this assignment.
 
-This is where I have currently become stuck, although I hoping to receive some assistance from a tutor and get the app up and running very soon. Please feel free to grade accordingly. I'll re-submit once finished!
+From here I continued to work on the functions in my script and thought about how to create my DOM elements. I did find a tutorial online that helped with this, however I had to do some serious editing and manipulating to add the buttons I wanted to create for visual appeal. The tutorial had only helped create regular list items with innerHTML. I was able to first create the list items then append a child to reach "empty" list item. Because I started on CSS first and had already created and deleted filler elements in HTML, I already knew how everything would look.
+
+After I had the site rendering the HTML I needed, I got started on some funtions to help me compare right and wrong answers, as well as penalize the user for wrong answers. For fun, I added some sound effects. These had to first be linked through the HTML but were referenced using JS variables and "getElementByID".
+
+I'm much farther along than I was before and am going to be doing another push to my repository. I may or may not be done with the highscore page by then, however I'm quite happy with what I have so far regardless. If I have time after completing the highscore page, I may search for better questions / add more to the quiz. Enjoy!
 
 ---------------------------------------------------------
 https://github.com/xander-deanna/code-quiz
